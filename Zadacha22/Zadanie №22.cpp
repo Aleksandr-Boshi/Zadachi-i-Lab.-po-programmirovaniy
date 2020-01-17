6d3c8a58b84797ce563ca77e808250dddc7d0f00
Aleksandr-Boshi <59053045+Aleksandr-Boshi@users.noreply.github.com>
2020-01-17 19:59:16 +0300

    Add files via upload

---
+++ Zadacha22/Zadanie №22.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+using namespace std;
+int o = 1;
+
+void swap(int* a, int* b) {
+
+	int k;
+	k = *a;
+	*a = *b;
+	*b = k;
+}
+
+void info(const int* p) {
+	cout << "Адрес:" << p << "; Значение:" << *p << endl;
+}
+
+int msort(int* a, int* b, int* c) {
+	if ((*a >= *b) && (*a >= *c)) {
+		return *a;
+	}
+	else if ((*b >= *a) && (*b >= *c)) {
+		return *b;
+	}
+	else return *c;
+
+}
+
+int* add(int* a, const int* p) {
+	*a = *a + *p;
+	return a;
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	int a = 1, b = 2, c = 3;
+
+	info(&a);
+	info(&b);
+	info(&c);
+	cout << "Максимум:" << msort(&a, &b, &c);
+	cout << endl;
+	add(&a, &b);
+
+	info(add(&a, &c));
+}
