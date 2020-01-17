1cac79c99d39a809dd3e9703256217c3014293fe
Aleksandr-Boshi <59053045+Aleksandr-Boshi@users.noreply.github.com>
2020-01-17 18:02:20 +0300

    Add files via upload

---
+++ Zadacha16/Zadanie №16.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+bool cool(long long int a) {
+
+	for (int i = 2; i < a; i++) {
+		if (a % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+bool factorization(long long int a, string& result_str) {
+	int k = 1;
+
+	for (int i = 2; i <= a; i++) {
+		if (a % i == 0) {
+			k = 1;
+			a /= i;
+				while (a % i == 0) {
+					k++;
+					a /= i;
+				}
+				if (k != 1) result_str += to_string(i) + "^" + to_string(k) + "*";
+					else result_str += to_string(i) + "*";
+		}
+	}
+	result_str[result_str.length() - 1] = '\0';
+	return true;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	long long int num;
+	string result;
+	int k = 1;
+
+		cout << "Введите число для факторизации(например 99): ";
+		cin >> num;
+
+	factorization(num, result);
+
+		cout << result;
+
+	return 0;
+}
