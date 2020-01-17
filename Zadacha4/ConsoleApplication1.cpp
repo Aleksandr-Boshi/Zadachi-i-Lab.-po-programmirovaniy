eaf2c7a09ec3c7e73e3f69cddee1c5856ddc1efd
Aleksandr-Boshi <59053045+Aleksandr-Boshi@users.noreply.github.com>
2020-01-17 17:09:34 +0300

    Add files via upload

---
+++ Zadacha4/ConsoleApplication1.cpp
@@ -0,0 +1,42 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{	  
+	setlocale(0, "");
+
+	int a, b;
+
+	cout << "Введите первое число: ";
+cin >> a;
+
+	cout << "Введите второе число: ";
+cin >> b;
+
+a = a + b;
+
+b = a - b;
+
+a = a - b;
+
+
+cout << a << endl;
+
+cout << b << endl;
+
+
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
